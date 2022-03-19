437d524b13a602858f1ff6a950b307563f96b682
clickingbuttons <clickingbuttons@pm.me>
2022-03-18 22:26:06 -0400

    maple review

--- lib/hashmap.h
+++ lib/hashmap.h
@@ -15,8 +15,8 @@ typedef struct hashmap {
   size_t val_size;
   size_t capacity;
   size_t nmemb;
-  void* zero_key; // used for testing if row is available
   char* data;
+  void* empty_key;
 } hashmap;
 
 #define hm_init(key_type, val_type) _hm_init(sizeof(key_type), sizeof(val_type))
@@ -27,9 +27,9 @@ static hashmap _hm_init(size_t key_size, size_t val_size) {
     .val_size = val_size,
     .capacity = HASHMAP_DEFAULT_CAPACITY,
     .nmemb = 0,
-    .zero_key = calloc(1, key_size),
-    .data = (char*)calloc(HASHMAP_DEFAULT_CAPACITY, key_size + val_size),
+    .data = (char*)calloc(HASHMAP_DEFAULT_CAPACITY + 1, key_size + val_size),
   };
+  res.empty_key = res.data + HASHMAP_DEFAULT_CAPACITY;
   return res;
 }
 
@@ -53,21 +53,10 @@ static inline char* hm_get_val(hashmap* hm, size_t index) {
   return hm_get_key(hm, index) + hm->key_size;
 }
 
-static inline char* hm_find_key_at(hashmap* hm, void* key, size_t start) {
-  char* res = hm_get_key(hm, start);
-  for (int i = 1; memcmp(res, key, hm->key_size) != 0; i++) {
-    start += i * i;
-    start = start & (hm->capacity - 1);
-    res = hm_get_key(hm, start);
-  }
-
-  return res;
-}
-
 static void _hm_put(hashmap* hm, void* key, void* val) {
   uint64_t index = wyhash(key, hm->key_size) & (hm->capacity - 1);
 
-  if (memcmp(hm->zero_key, key, hm->key_size) == 0) {
+  if (memcmp(hm->empty_key, key, hm->key_size) == 0) {
     perror("cannot write 0 key");
     return;
   }
@@ -80,7 +69,12 @@ static void _hm_put(hashmap* hm, void* key, void* val) {
   if (memcmp(existing_key, key, hm->key_size) == 0) { // overwrite
     memcpy(existing_key + hm->key_size, val, hm->val_size);
   } else { // insert
-    char* offset = hm_find_key_at(hm, hm->zero_key, index);
+    char* offset = hm_get_key(hm, index);
+    for (int i = 1; memcmp(offset, hm->empty_key, hm->key_size) != 0; i++) {
+      index += i * i;
+      index = index & (hm->capacity - 1);
+      offset = hm_get_key(hm, index);
+    }
     memcpy(offset, key, hm->key_size);
     memcpy(offset + hm->key_size, val, hm->val_size);
     hm->nmemb += 1;
@@ -91,10 +85,11 @@ static void hm_grow(hashmap* hm) {
   size_t old_capacity = hm->capacity;
   char* old_data = hm->data;
   hm->capacity *= 2;
-  hm->data = (char*)calloc(hm->capacity, hm->key_size + hm->val_size);
+  hm->data = (char*)calloc(hm->capacity + 1, hm->key_size + hm->val_size);
+  hm->empty_key = hm->data + hm->capacity;
   for (int i = 0; i < old_capacity; i++) {
     char* key = hm_get_key_at(hm, old_data, i);
-    if (memcmp(key, hm->zero_key, hm->key_size) == 0) {
+    if (memcmp(key, hm->empty_key, hm->key_size) == 0) {
       continue;
     }
     _hm_put(hm, key, key + hm->key_size);
@@ -109,20 +104,30 @@ static void hm_grow(hashmap* hm) {
 
 static void* _hm_get(hashmap* hm, void* key) {
   uint64_t index = wyhash(key, hm->key_size) & (hm->capacity - 1);
-  char* offset = hm_find_key_at(hm, key, index);
+  char* offset = hm_get_key(hm, index);
+  if (memcmp(offset, hm->empty_key, hm->key_size) == 0) {
+    return NULL;
+  }
+  for (int i = 1; memcmp(offset, key, hm->key_size) != 0; i++) {
+    index += i * i;
+    index = index & (hm->capacity - 1);
+    offset = hm_get_key(hm, index);
+    if (memcmp(offset, hm->empty_key, hm->key_size) == 0) {
+      return NULL;
+    }
+  }
 
   return offset + hm->key_size;
 }
 
 static void hm_free(hashmap* hm) {
   free(hm->data);
-  free(hm->zero_key);
 }
 
 static void hm_print(hashmap* hm) {
   for (int i = 0; i < hm->capacity; i++) {
     char* key = hm_get_key(hm, i);
-    if (memcmp(key, hm->zero_key, hm->key_size) != 0) {
+    if (memcmp(key, hm->empty_key, hm->key_size) != 0) {
       for (int j = 0; j < hm->key_size; j++) {
         printf("%02hhx", *(key + j));
         if (j != hm->key_size - 1) {
@@ -150,5 +155,5 @@ static void* hm_end(hashmap* hm) {
 }
 
 static bool hm_exist(hashmap* hm, size_t index) {
-  return memcmp(hm_get_key(hm, index), hm->zero_key, hm->key_size) != 0;
+  return memcmp(hm_get_key(hm, index), hm->empty_key, hm->key_size) != 0;
 }
