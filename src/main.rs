8c0da4f4313f9dff642c3142962fc6abd4bb1abd
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-25 19:57:13 -0400

    simple server

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,59 @@ use nix::{
 use std::{
   env::var,
   io::{Read, Write},
-  net::TcpListener,
+  net::{TcpStream, TcpListener},
   process::exit
 };
+mod server;
+use server::query::handle_query;
 
 const PORT: &str = "8080";
-const BUFFER_SIZE: usize = 2 << 15;
+const BUFFER_SIZE: usize = 1 << 16;
 
 extern "C" fn handle_sigint(_: i32) { exit(0); }
 
-fn handle_request<'a>(req: &Request) -> Response<&'a [u8]> {
+fn handle_request(req: &Request, body: &[u8]) -> Response<String> {
   let builder = Response::builder();
   match req.method {
     None => builder
       .status(400)
-      .body("expected request method\n".as_bytes())
+      .body("expected request method\n".to_string())
       .unwrap(),
     Some(m) => match m {
-      "GET" => builder.body("tickdb here\n".as_bytes()).unwrap(),
-      "POST" => builder.body("post\n".as_bytes()).unwrap(),
-      _ => builder.status(404).body("not found\n".as_bytes()).unwrap()
+      "GET" => builder.body("tickdb here\n".to_string()).unwrap(),
+      "POST" => {
+				let resp = handle_query(req, body);
+				builder.body(resp).unwrap()
+			},
+      _ => builder.status(404).body("not found\n".to_string()).unwrap()
     }
   }
+}
+
+fn write_http_header(stream: &mut TcpStream, response: &Response<String>) {
+	let mut header = format!(
+		"{:?} {} {}\r\nContent-Length: {}\r\n",
+		response.version(),
+		response.status().as_str(),
+		response.status().canonical_reason().unwrap_or(""),
+		response.body().len()
+	);
+	for (key, val) in response.headers() {
+		header.push_str(&format!("{}: {}\r\n", key, val.to_str().unwrap()));
+	}
+	header.push_str("\r\n");
+	stream.write(header.as_bytes()).unwrap();
 }
 
 fn main() {
   let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT)).unwrap();
 
-  let num_threads = var("ZDB_NUM_THREADS")
+  let num_procs = var("TICKDB_NUM_PROCS")
     .unwrap_or("2".to_string())
     .parse::<i64>()
     .unwrap();
 
-  for i in 0..num_threads {
+  for i in 0..num_procs {
     match unsafe { fork() } {
       Ok(ForkResult::Child) => {
         println!("fork {}", i);
@@ -51,27 +71,19 @@ fn main() {
         unsafe {
           signal::sigaction(signal::SIGINT, &sig_action).unwrap();
         }
-        let mut buffer = [0; BUFFER_SIZE];
+        let buffer = &mut [0; BUFFER_SIZE];
         for stream in listener.incoming() {
           let mut headers = vec![httparse::EMPTY_HEADER; 20];
-          let mut stream = stream.unwrap();
-          stream.read(&mut buffer).unwrap();
+          let stream = &mut stream.unwrap();
+          let request_size = stream.read(buffer).unwrap();
 
           let mut request = Request::new(&mut headers[..]);
-          request.parse(&mut buffer).unwrap();
+          let body_offset: usize = request.parse(buffer).unwrap().unwrap();
+					let body = &buffer[body_offset..request_size];
 
-          let response = handle_request(&request);
-          let statusline = format!(
-            "{:?} {} {}\r\nContent-Length: {}\r\n",
-            response.version(),
-            response.status().as_str(),
-            response.status().canonical_reason().unwrap_or(""),
-						response.body().len()
-          );
-          stream.write(statusline.as_bytes()).unwrap();
-					//println!("{:?}", response.headers());
-					stream.write(b"\r\n").unwrap();
-          stream.write(response.body()).unwrap();
+          let response = handle_request(&request, body);
+					write_http_header(stream, &response);
+          stream.write(response.body().as_bytes()).unwrap();
         }
       }
       Ok(ForkResult::Parent { child: _ }) => {}
---
+++ src/server/query.rs
@@ -0,0 +1,94 @@
+use serde::{de, Deserialize};
+use httparse::Request;
+use chrono::{DateTime, NaiveDate};
+use std::io::{Error, ErrorKind};
+
+#[derive(Deserialize)]
+pub struct Query {
+	pub table: String,
+  #[serde(deserialize_with = "string_to_datetime")]
+  pub from:  i64,
+  #[serde(deserialize_with = "string_to_datetime")]
+  pub to:    i64,
+  pub query: String,
+}
+
+pub fn run_query<'a>(query: &Query) -> std::io::Result<String> {
+	Ok(format!("{} {} {} {}", query.table, query.from, query.to, query.query))
+}
+
+pub fn handle_query<'a>(_req: &Request, query: &[u8]) -> String {
+	match serde_json::from_slice::<Query>(query) {
+		Err(err) => format!("error parsing query: {}\n", err.to_string()),
+		Ok(mut query) => match run_query(&mut query) {
+			Ok(_value) => format!("query response\n"),
+			Err(err) => format!("error running query: {}\n", err.to_string()),
+		}
+	}
+}
+
+static NICE_FORMAT: &str = "%Y-%m-%d";
+pub fn string_to_nanoseconds(value: &str) -> std::io::Result<i64> {
+  // Nanoseconds since epoch?
+  if value.len() > 4 {
+    let nanoseconds = value.parse::<i64>();
+    if let Ok(nanoseconds) = nanoseconds {
+      return Ok(nanoseconds);
+    }
+  }
+  // TODO: check date is in valid range before calling timestamp_nanos
+  match DateTime::parse_from_rfc3339(&value) {
+    Ok(date) => Ok(date.timestamp_nanos()),
+    Err(_e) => match NaiveDate::parse_from_str(&value, &NICE_FORMAT) {
+      Ok(date) => Ok(date.and_hms(0, 0, 0).timestamp_nanos()),
+      Err(_e) => {
+        let msg = format!(
+          "Could not parse {} in RFC3339 or {} format",
+          &value, &NICE_FORMAT
+        );
+        Err(Error::new(ErrorKind::Other, msg))
+      }
+    }
+  }
+}
+
+fn string_to_datetime<'de, D>(deserializer: D) -> Result<i64, D::Error>
+where
+  D: de::Deserializer<'de>
+{
+  struct StringVisitor;
+
+  impl<'de> de::Visitor<'de> for StringVisitor {
+    type Value = i64;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+      formatter.write_str("a rfc 3339 string")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+      E: de::Error
+    {
+      match string_to_nanoseconds(value) {
+        Ok(nanos) => Ok(nanos),
+        Err(e) => Err(E::custom(e))
+      }
+    }
+
+    fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+    where
+      E: de::Error
+    {
+      Ok(value as i64)
+    }
+
+    fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+    where
+      E: de::Error
+    {
+      Ok(value)
+    }
+  }
+  deserializer.deserialize_any(StringVisitor)
+}
+
